85214c05ccb8a7c0026dc54f0aadab67de4ab52c
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-09-21 22:36:52 +0530

    Time: 235 ms (23.35%), Space: 46.8 MB (15.66%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        // if(nums.size()<=1) return nums[0];
+        // map<int,int> mp;
+        // for(int i=0;i<nums.size();i++) mp[nums[i]]++;
+        // vector<int> v;
+        // int pos,maxx=INT_MIN;
+        // for(auto x:mp){
+        //     if(x.second>1 && x.first%2==0){
+        //         v.push_back(x.first);
+        //     }
+        // }
+        // sort(v.begin(),v.end());
+        // if(v.size()==0) return -1;
+        // return v[0];
+        int ans = -1, mx = 0;
+        map<int, int> m;
+        for (auto &x : nums) m[x]++;
+        for (auto &x : m) {
+            if (x.first % 2 == 0 && x.second > mx) {
+                mx = x.second;
+                ans = x.first;
+            }
+        }
+        return ans;
+    }
+};
