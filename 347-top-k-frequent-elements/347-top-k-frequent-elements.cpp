3b624edf36317bc64651c1ee3072527d454c276d
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-09-24 13:30:28 +0530

    Time: 38 ms (19.17%), Space: 13.7 MB (73.04%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++) mp[nums[i]]++;
+        vector<pair<int,int>> v;
+        for(auto x:mp) v.push_back({x.second,x.first});
+        sort(v.begin(),v.end(),greater<pair<int,int>>());
+        vector<int> res;
+        for(int i=0;i<min(k,(int)v.size());i++) res.push_back(v[i].second);
+        return res;
+    }
+};
