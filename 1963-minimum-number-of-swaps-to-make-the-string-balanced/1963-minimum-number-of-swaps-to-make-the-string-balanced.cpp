7819d059123c6ce144c9f3e73c6aceaeb0bae121
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-10-08 19:50:50 +0530

    Time: 550 ms (5.20%), Space: 33.7 MB (17.69%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -3,16 +3,23 @@ public:
     int minSwaps(string s) {
         stack<char> st;
         for(auto x:s){
-            if(x=='[') 
-                st.push(x);
-            else if(!st.empty())
-                st.pop();
+            if(!st.empty() && st.top()=='[' && x==']')
+                    st.pop();
+                else if(x=='[')
+                    st.push(x);
         }
         // while(!st.empty()){
         //    cout<<st.top()<<" ";
         //     st.pop();
         // }
         //cout<<st.size();
+        if(st.size()%2==0) return st.size()/2;
         return (st.size()+1)/2;
     }
-};
+}; 
+    /*
+    ]]][[[
+    ]]]
+    [[[
+    
+    */
