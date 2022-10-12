946eae7e96adbe6fe58b7a7213e4d74e7b16aee8
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-10-12 22:06:27 +0530

    Added solution - LeetHub

--- Decode the string - GFG/decode-the-string.cpp
+++ Decode the string - GFG/decode-the-string.cpp
@@ -10,29 +10,28 @@ using namespace std;
 class Solution{
 public:
     string decodedString(string s){
-        stack<char>st;
-        string res,ans;
+        stack<char> st;
         for(int i=0;i<s.size();i++){
             if(s[i]!=']') st.push(s[i]);
-            else
-            {   
+            else{
                 string ans="";
                 while(st.top()!='['){
                     ans=st.top()+ans;
                     st.pop();
                 }
                 st.pop();
-                string numb="";
+                string num="";
                 while(!st.empty() && isdigit(st.top())){
-                    numb=st.top()+numb;
+                    num=st.top()+num;
                     st.pop();
-                }
-                int n=stoi(numb);
+                }// num="2";
+                int n=stoi(num);
                 string temp=ans;
-                for(int i=0;i<n-1;i++) ans=ans+temp;
-                for(int i=0;i<ans.size();i++)   st.push(ans[i]);
+                for(int i=1;i<n;i++) ans=ans+temp;
+                for(int i=0;i<ans.size();i++) st.push(ans[i]);
             }
         }
+        string res;
         while(!st.empty()){
             res=st.top()+res;
             st.pop();
@@ -40,6 +39,7 @@ public:
         return res;
     }
 };
+
 
 //{ Driver Code Starts.
 
