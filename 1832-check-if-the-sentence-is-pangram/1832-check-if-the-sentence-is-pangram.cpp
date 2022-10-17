39662795a33036968155412c291bd8877e890550
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-10-17 18:59:10 +0530

    Time: 8 ms (28.88%), Space: 6.7 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -3,12 +3,12 @@ public:
     bool checkIfPangram(string s) {
         map<char,int> mp;
         for(auto x:s) mp[x]++;
-        // vector<char> v;
-        // for(auto x:mp) v.push_back(x.first);
-        for(int i=0;i<26;i++){
-            if(mp['a'+i]==0) return 0;
-        }
-        //if(v.size()==26) return 1;
-        return 1;
+        vector<char> v;
+        for(auto x:mp) v.push_back(x.first);
+        // for(int i=0;i<26;i++){
+        //     if(mp['a'+i]==0) return 0;
+        // }
+        if(v.size()==26) return 1;
+        return 0;
     }
 };
