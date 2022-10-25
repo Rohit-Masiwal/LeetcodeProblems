5811c3b2f5b46b0e9a8fcd73d14c8bdf7caea5e5
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-10-25 09:11:44 +0530

    Time: 11 ms (38.92%), Space: 11.5 MB (56.29%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -4,8 +4,6 @@ public:
         string w1,w2;
         for(auto x:word1) w1+=x;
         for(auto x:word2) w2+=x;
-        // sort(w1.begin(),w1.end());
-        // sort(w2.begin(),w2.end());
         return w1==w2;
     }
 };
