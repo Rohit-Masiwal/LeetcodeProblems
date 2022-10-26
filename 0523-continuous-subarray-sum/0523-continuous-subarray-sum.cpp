f448c6215e619ad919494fec0aa00e3e20443275
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-10-26 10:45:46 +0530

    Time: 562 ms (23.77%), Space: 115 MB (19.60%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        // int sum=0;
-        // if(nums.size<2) return 0;
-        // for(auto x:nums) sum+=x;
-        // if(sum%k==0) return 1;
-        // for(int i=0;i<nums.size()-1;i++){
-        //     if(nums[i]+nums[i+1]==k) return 1;
-        // }
         int sum=0;
         map<int,int> mp;
         mp[0]=-1;
