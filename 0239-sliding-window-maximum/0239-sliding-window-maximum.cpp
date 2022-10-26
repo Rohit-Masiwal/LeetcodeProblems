d78f6f399f2ed98c848b3663a72077a33eb2c155
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-10-26 18:07:20 +0530

    Time: 597 ms (56.20%), Space: 134.6 MB (81.07%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,9 +4,9 @@ public:
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && dq.front()==i-k) dq.pop_front();
             while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
             dq.push_back(i);
+            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
             if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
         return ans;
