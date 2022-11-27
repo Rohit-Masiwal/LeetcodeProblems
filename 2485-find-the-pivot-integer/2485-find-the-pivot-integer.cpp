e45d4a4da0b8f29eb0cb3b613397a79518c39120
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2022-11-27 11:45:00 +0530

    Time: 381 ms (9.09%), Space: 5.8 MB (100.00%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum=n*(n+1)/2;
+        int sum1=0,sum2=0;
+        for(int i=1;i<=n;i++){
+            sum1+=i;
+            /*
+            s=32
+            s1=1,3,6
+            s2=32-1-1=30,32-3-2=27,32-6-3=21
+            */
+            sum2=sum-sum1+i;
+            cout<<sum1<<" "<<sum2<<"\n";
+            if(sum1==sum2) return i;
+        }
+        return -1;
+    }
+};
