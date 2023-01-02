e8779fcb2f831ae4b4ab93f172de226c7e99f14b
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2023-01-02 21:25:51 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (24.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cnt =0;
+        for(int i=0; i<word.size(); i++){
+            if(isupper(word[i])) cnt++;
+        }
+        if((cnt == word.size()) || (cnt == 0) || (cnt == 1 && isupper(word[0]))){   
+            return true; 
+        }
+        return false;
+    }
+};
