b8cc3a673c155365cf45e507db3c7b75d79e00e6
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2023-01-27 19:01:20 +0530

    Time: 403 ms (45.25%), Space: 119.2 MB (38.18%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_map<string, int> mp;
+        for(string word: words){
+            mp[word]++;
+        }
+
+        vector<string> ans;
+        for(string word: words){
+            int n= word.size();
+            vector<int> dp(n+1,0);
+            dp[0]=1;
+            for(int i=0; i<n; i++){
+                if(!dp[i]){
+                    continue;
+                }
+                for(int j = i+1 ; j<=n ;j++){
+                    if(j-i<n && mp[(word.substr(i, j-i))]>0){
+                        dp[j]=1;
+                    }
+                }
+                if(dp[n]){
+                    ans.push_back(word);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
