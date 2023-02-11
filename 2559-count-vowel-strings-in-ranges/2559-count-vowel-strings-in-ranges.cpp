5f20093a3cf8625f0c422aa89ffb0487d5421a09
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2023-02-11 20:48:29 +0530

    Time: 153 ms (68.35%), Space: 64 MB (64.85%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool check(char c){
-        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ) return 1;
+        if(c=='a' ||c=='e' ||c=='i' ||c=='o' ||c=='u') return 1;
         return 0;
     }
-    vector<int> vowelStrings(vector<string>& w, vector<vector<int>>& x) {
-        vector<int> ans;
+    vector<int> vowelStrings(vector<string>& w, vector<vector<int>>& q) {
         int n=w.size();
+        vector<int> ans;
         vector<int> res(n);
         for(int i=0;i<n;i++){
-            if(check(w[i][0]) && check(w[i][w[i].size()-1])) {
-                if(i==0) res[0]=1;
-                else res[i]=res[i-1]+1;
-            }
-            else {
+            if(check(w[i][0]) && check(w[i][w[i].size()-1])){
+                if(i==0) 
+                    res[0]=1;
+                    else res[i]=res[i-1]+1;      
+            }else{
                 if(i==0) res[0]=0;
                 else res[i]=res[i-1];
             }
         }
-        for(auto x:res) cout<<x<<" ";
-        for(int i=0;i<x.size();i++) {
-            int l=x[i][0]; //queries[0][0]
-            int r=x[i][1]; //x[]
+        for(int i=0;i<q.size();i++){
+            int l=q[i][0];
+            int r=q[i][1];
             if(l==0) ans.push_back(res[r]);
             else ans.push_back(res[r]-res[l-1]);
         }
