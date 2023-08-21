b149c75e8d3778b46ff0d2e1d552de679b6a6e60
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2023-08-21 10:15:53 +0530

    Time: 783 ms (7.82%), Space: 11.2 MB (87.08%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t=s;
+        for(int i=0;i<s.size()-1;i++) {
+            t.push_back(t[0]);
+            t.erase(0,1);
+            if(t==s) return 1;
+        }
+        return 0;
+    }
+};
