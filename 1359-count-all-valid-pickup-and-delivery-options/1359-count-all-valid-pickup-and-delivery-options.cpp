24840dab5e8704963bc89877815da726ef8b5d98
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2023-09-10 18:35:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.89%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        if(n==1) return 1;
+        long long ans=1;
+        int M=1000000007;
+        for(int i=2;i<=n;i++) {
+            int space = (i-1)*2+1;
+            int possibility = space*(space+1)/2;
+            ans*=possibility;
+            ans%=M;
+        }
+        return ans;
+    }
+};
