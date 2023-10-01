2ae9f775fdcc89d38256b983b4c5ec495b63426f
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2023-10-01 19:31:12 +0530

    Time: 10 ms (92.55%), Space: 13 MB (25.97%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // string ans, temp;
-        // int cnt=0;
-        // for(int i=0;i<s.size();i++) {
-        //     temp+=s[i];
-        //     if(s[i]==' ') {
-        //         // cout<<temp<<"\n";
-        //         reverse(temp.begin(),temp.end());
-        //         ans+=temp;
-        //         temp="";
-        //     }
-        // }
-        // //cout<<temp;
-        // ans+=" ";
-        // reverse(temp.begin(),temp.end());
-        // ans+=temp;
-        // return ans;
+        string ans, temp;
         int cnt=0;
         for(int i=0;i<s.size();i++) {
+            temp+=s[i];
             if(s[i]==' ') {
-                reverse(s.begin()+cnt,s.begin()+i);
-                cnt=i+1;
+                // cout<<temp<<"\n";
+                reverse(temp.begin(),temp.end());
+                ans+=temp;
+                temp="";
             }
         }
-        reverse(s.begin()+cnt,s.end());
-        return s;
+        //cout<<temp;
+        ans+=" ";
+        reverse(temp.begin(),temp.end());
+        ans+=temp;
+        string res;
+        //cout<<ans;
+        for(int i=1;i<ans.size();i++) {
+            res+=ans[i];
+        }
+        return res;
+        // int cnt=0;
+        // for(int i=0;i<s.size();i++) {
+        //     if(s[i]==' ') {
+        //         reverse(s.begin()+cnt,s.begin()+i);
+        //         cnt=i+1;
+        //     }
+        // }
+        // reverse(s.begin()+cnt,s.end());
+        // return s;
     }
 };
