1696bdc0c86d847218aad42b681f426d3eecb4d4
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2024-01-02 21:21:45 +0530

    Time: 37 ms (5.35%), Space: 30 MB (70.96%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -3,20 +3,16 @@ public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         unordered_map<int,int> ump;
         for(auto x:nums) ump[x]++;
-        vector<vector<int>> ans;
-        //for(auto x:ump) cout<<x.first<< " "<< x.second<<"\n";
-        int maxx=0;
-        for(auto x:ump) maxx=max(maxx,x.second);
-        for(int i=0;i<maxx;i++) {  
-            vector<int> v;
-            for(auto& x:ump) {
-                if(x.second>0) {
-                    v.push_back(x.first);
-                    x.second--;
-                }
+        for(auto x:ump) cout<<x.first<< " "<< x.second<<"\n";
+        int m=0;
+        for(auto x:ump) {  
+            m=max(m,x.second);
+        }
+        vector<vector<int>> ans(m,vector<int>());
+        for(auto x:ump) {
+            for(int i=0;i<x.second;i++) {
+                ans[i].push_back(x.first);
             }
-            //for(auto x:v) ump[x]--;
-            ans.push_back(v);
         }
         return ans;
     }
