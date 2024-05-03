04d0acdca3f0dbef1dcc6ec0022c4b7b886086a5
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2024-05-03 13:49:09 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (93.86%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -8,16 +8,13 @@ public:
              
             while(j<n2 && version2[j]!='.') num2 = num2*10+(version2[j++]-'0');
                 
-            if(num1>num2)
-            {
-                return 1;
-            }
-            if(num1 < num2) 
-            {
-                return -1;
-            }
+            if(num1>num2) return 1;
+            
+            if(num1 < num2) return -1;
+            
             i++;
             j++;
+            
             num1 = 0;
             num2 = 0;
         }
