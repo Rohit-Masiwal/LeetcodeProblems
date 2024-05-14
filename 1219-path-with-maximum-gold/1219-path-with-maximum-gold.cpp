ead8366d95950aff92f71b2af8fb8da47b2e7348
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2024-05-14 20:51:16 +0530

    Time: 98 ms (87.10%), Space: 9.1 MB (81.32%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,28 +1,29 @@
-int dfs(vector<vector<int>>& grid, int i,int j)
-{
-    
-    int ans=0, temp=0;
-    if(i>=grid.size() || i<0 || j<0 || j>=grid[0].size() || grid[i][j]==0) return 0;
-    temp=grid[i][j];
-    grid[i][j]=0;
-    ans = max(ans,dfs(grid,i-1,j));
-    ans = max(ans,dfs(grid,i+1,j));
-    ans = max(ans,dfs(grid,i,j+1));
-    ans = max(ans,dfs(grid,i,j-1));
-    grid[i][j] = temp;
-    return ans+temp;
-}
-
 class Solution {
 public:
-    int getMaximumGold(vector<vector<int>>& grid){
+    int dfs(vector<vector<int>>& grid ,int i, int j) {
+        int n=grid.size();
+        int m=grid[0].size();
+        if(i>=n || j>=m || i<0 || j<0 || grid[i][j]==0) return 0;
         
+        int temp=grid[i][j];
+        grid[i][j]=0;
         int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]>0){
-                    ans = max(ans,dfs(grid,i,j));
-                }
+        
+        ans=max(ans,dfs(grid,i-1,j));
+        ans=max(ans,dfs(grid,i,j-1));
+        ans=max(ans,dfs(grid,i+1,j));
+        ans=max(ans,dfs(grid,i,j+1));
+        
+        grid[i][j]=temp;
+        return ans+temp;
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j]!=0) ans=max(ans,dfs(grid,i,j));
             }
         }
         return ans;
