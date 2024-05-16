8cfff2a6c26947420a384888233b5cd3e742d010
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2024-05-16 09:40:21 +0530

    Time: 8 ms (82.42%), Space: 17.4 MB (64.84%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -11,20 +11,10 @@
  */
 class Solution {
 public:
-    // void Inorder(TreeNode* root, vector<int> &v) {
-    //     if(root){
-    //         Inorder(root->left, v);
-    //         v.push_back(root->val);
-    //         Inorder(root->right, v);
-    //     }
-    // }
     bool evaluateTree(TreeNode* root) {
-        if(!root->left && !root->right) {
-            if(root->val) return true;
-            return false;
-        }
-        bool l=evaluateTree(root->left);
-        bool r=evaluateTree(root->right);
+        if(!root->left && !root->right) return root->val;
+        int l=evaluateTree(root->left);
+        int r=evaluateTree(root->right);
         if(root->val==2) return l|r;
         return l&r;
     }
