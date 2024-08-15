5d7eee49f1e29958d7c9d4a3472d1b779000562c
Rohit Masiwal <88874168+Rohit-Masiwal@users.noreply.github.com>
2024-08-15 11:39:50 +0530

    Time: 75 ms (43.55%), Space: 86.1 MB (32.71%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -3,15 +3,11 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int five=0, ten=0;
         for(int i=0;i<bills.size();i++) {
-            if(bills[i]==5) {
-                five++;
-            }
+            if(bills[i]==5) five++;
             else if(bills[i]==10) {
                 ten++;
                 if(five==0) return false;
-                else {
-                    five--;
-                }
+                else five--;
             } else {
                 if(ten>=1) {
                     if(five>=1) {
@@ -20,9 +16,7 @@ public:
                     }
                     else return false;
                 }
-                else if(five>=3) {
-                    five-=3;
-                }
+                else if(five>=3) five-=3;
                 else return false;
             }
         }
